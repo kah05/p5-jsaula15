3afee2ee9dcdb2b986c6e2d00629058694996b25
kah05 <137062347+kah05@users.noreply.github.com>
2023-08-18 08:48:46 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+   background("rgb(0,255,179)");
+}
+
+function draw() {
+  
+  stroke("BLUE");
+  fill("rgb(81,81,255)");
+  
+ if(mouseIsPressed);
+  rect(mouseX,mouseY,20,20);
+} 
